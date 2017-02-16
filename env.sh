1da05004bbccff943d1dcca8381a132a9a5db189
Junjie <junjiejiang94@gmail.com>
2017-02-15 21:33:50 -0800

    Delete env.sh

--- env.sh
+++
@@ -1,8 +0,0 @@
-export FACEBOOK_APP_ID='714255962085965'
-export FACEBOOK_APP_SECRET='cb622f707d0c5aa78a4b1a5af29f5c10'
-export region="us-west-2"
-export GOOGLE_CLIENT_ID="199375453961-1h06ptnlk3mot443l5lso825bkia8307.apps.googleusercontent.com"
-export GOOGLE_CLIENT_SECRET="i0Ob5UOeQeX9FuBDyXDtPGVX"
-export secret=""
-export User="AKIAIV2JQCV3BSRJ6L3Q"
-export Storage="RjACohpRnM/IXwTU6vB6pdxuELIFh3NJ1sV7qY2Q"
